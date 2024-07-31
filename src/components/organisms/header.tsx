bd9cfdd4379e28bee65867f1dfba9e5d72329510
Abner Oliveira <bnelce@yahoo.com.br>
2024-07-31 18:54:03 -0300

    header links

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -5,12 +5,16 @@ const Header: React.FC = () => {
     <div className="flex flex-col">
       <header className="bg-white shadow">
         <nav className="container mx-auto px-6 py-3 flex justify-between items-center">
-          <div className="flex flex-row items-center gap-2 text-[#00DF7C] font-bold text-xl lg:text-2xl">
-            <HeaderLogo /> Clarke Energia Challenge
-          </div>
-          {/* <Nav /> */}
           <a
-            href="#"
+            className="flex flex-row items-center gap-2 text-[#00DF7C] font-bold text-xl lg:text-2xl"
+            href="/"
+          >
+            {" "}
+            <HeaderLogo /> Clarke Energia Challenge
+          </a>
+          <a
+            href="https://clarke.com.br/seja-cliente/"
+            target="_blank"
             className="bg-[#00DF7C] hover:bg-green-700 text-white py-2 px-4 rounded hidden lg:block"
           >
             Seja nosso cliente
